d42640620e866eb47acfe229250c5ae28d276b01
raguirre <raguirre@blastasia.com>
2016-11-03 16:00:52 +0800

    Added InterviewCake.AppleStocks problem and solution

---
+++ TestSuite.InterviewCake.Test/AppleStocks/SolutionTest.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+using TestSuite.InterviewCake.AppleStocks;
+
+namespace TestSuite.InterviewCake.Test.AppleStocks
+{
+
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        public void TestEmpty()
+        {
+            // Arrange
+            var input = new int[0];
+
+            // Act
+            var result = Solution.GetMaxProfit(input);
+
+            // Assert
+            result.ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void TestSingle()
+        {
+            // Arrange
+            var input = new int[] { 99 };
+
+            // Act
+            var result = Solution.GetMaxProfit(input);
+
+            // Assert
+            result.ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void TestLoss()
+        {
+            // Arrange
+            var input = new int[] { 7, 5 };
+
+            // Act
+            var result = Solution.GetMaxProfit(input);
+
+            // Asser
+            result.ShouldEqual(-2);
+        }
+
+        [TestMethod]
+        public void TestWin()
+        {
+            // Arrange
+            var input = new int[] { 5, 7 };
+
+            // Act
+            var result = Solution.GetMaxProfit(input);
+
+            // Assert
+            result.ShouldEqual(2);
+        }
+
+        [TestMethod]
+        public void TestMultiple()
+        {
+            // Arrange
+            var input = new int[] { 10, 7, 5, 8, 11, 9 };
+
+            // Act
+            var result = Solution.GetMaxProfit(input);
+
+            // Assert
+            result.ShouldEqual(6);
+        }
+
+        [TestMethod]
+        public void TestMultiple_WhenHighestComesBeforeLowest()
+        {
+            // Arrange
+            var input = new int[] { 99, 10, 7, 5, 8, 11, 9 };
+
+            // Act
+            var result = Solution.GetMaxProfit(input);
+
+            // Assert
+            result.ShouldEqual(6);
+        }
+
+        [TestMethod]
+        public void TestMultiple_WhenAllAreLosses()
+        {
+            // Arrange
+            var input = new int[] { 99, 87, 54, 51, 20 };
+
+            // Act
+            var result = Solution.GetMaxProfit(input);
+
+            // Assert
+            result.ShouldEqual(-3);
+        }
+    }
+}
---
+++ TestSuite.InterviewCake/AppleStocks/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestSuite.InterviewCake.AppleStocks
+{
+    public static class Solution
+    {
+        public static int GetMaxProfit(params int[] profits)
+        {
+            if (profits.Length <= 1)
+                return 0;
+
+            var maxProfit = profits[1] - profits[0];
+            var minPrice = profits[0];
+            for (int i = 1; i < profits.Length - 1; i++)
+            {
+                maxProfit = Math.Max(maxProfit, profits[i] - minPrice);
+                minPrice = Math.Min(minPrice, profits[i]);
+            }
+
+            return maxProfit;
+        }
+    }
+}
