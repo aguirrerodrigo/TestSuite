a15c0482eb8fc1af8619b7948acf87104acc2274
raguirre <raguirre@blastasia.com>
2016-11-04 15:59:07 +0800

    Added ProductsExceptIndex problem and solution

---
+++ TestSuite.InterviewCake.Test/ProductsExceptIndex/SolutionTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+using TestSuite.InterviewCake.ProductExceptIndex;
+
+namespace TestSuite.InterviewCake.Test.ProductsExceptIndex
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        public void TestInput()
+        {
+            // Arrange
+            var input = new int[] { 1, 7, 3, 4 };
+
+            // Act
+            var result = Solution.GetProductsExceptIndex(input);
+
+            // Assert
+            result[0].ShouldEqual(84);
+            result[1].ShouldEqual(12);
+            result[2].ShouldEqual(28);
+            result[3].ShouldEqual(21);
+        }
+
+        [TestMethod]
+        public void TestWithZeroes()
+        {
+            // Arrange
+            var input = new int[] { 1, 7, 0, 3, 4 };
+
+            // Act
+            var result = Solution.GetProductsExceptIndex(input);
+
+            // Assert
+            result[0].ShouldEqual(0);
+            result[1].ShouldEqual(0);
+            result[2].ShouldEqual(84);
+            result[3].ShouldEqual(0);
+            result[4].ShouldEqual(0);
+        }
+    }
+}
---
+++ TestSuite.InterviewCake/ProductsExceptIndex/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace TestSuite.InterviewCake.ProductExceptIndex
+{
+    public static class Solution
+    {
+        public static int[] GetProductsExceptIndex(params int[] input)
+        {
+            var products = new int[input.Length];
+
+            var product = 1;
+            for (var i = 0; i < input.Length; i++)
+            {
+                products[i] = product;
+                product *= input[i];
+            }
+
+            product = 1;
+            for(var i = input.Length - 1; i >= 0; i--)
+            {
+                products[i] *= product;
+                product *= input[i];
+            }
+
+            return products;
+        }
+    }
+}
